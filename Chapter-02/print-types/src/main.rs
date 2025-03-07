a290b66a0f205bc99b007fea8ec82ef5a0839df8
Kim Andersen <wolfy@lycans.dk>
2025-03-07 13:40:55 +0100

    Updated after a few lectures.

--- Chapter-02/print-types/src/main.rs
+++ Chapter-02/print-types/src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    let s: &str = "Hello World!";
+
+    print!("{}\n", s);
+    println!("{}", s);
+
+    eprint!("{}\n", s);
+    eprintln!("{}\n", s);
+
+    let name: &str = "Kim";
+    println!("{name}");
+
+    let formatted_string: String = format!("Hello {name}, nice to meet you!");
+    println!("{formatted_string}");
 }
---
+++ Chapter-02/variables/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let mut x = 1337;
+
+    println!("Current value of x is: {x}");
+
+    x = -10;
+
+    println!("Current value of x is: {x}");
+}
